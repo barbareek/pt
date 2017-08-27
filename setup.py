8c166c48f162214e22fb4892da00fa5470374b1f
test <test@test.com>
2017-08-27 17:15:41 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pt',
+      version='0.1',
+      description='test how python package works',
+      url='https://github.com/barbareek/pt',
+      author='suraj',
+      author_email='suraj@test.com',
+      license='test',
+      packages=['pt'],
+      zip_safe=False)
